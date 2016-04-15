f8b54257a21bc2d525e7aca74df1b86b7ec827e7
teirm <tribunus.vii@gmail.com>
2016-04-15 16:19:58 -0400

    Setting up main loop for game

--- coffee_maker_quest/coffee_maker_quest.go
+++ coffee_maker_quest/coffee_maker_quest.go
@@ -1,17 +1,29 @@
 package coffee_maker_quest
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 /* Structs */
-type room struct {
+type Room struct {
 	north_door string
 	south_door string
 	furniture  string
 	item       string
 	room_adj   string
 }
+
+type Player struct {
+	current_pos int
+	win_status  int
+	bag_status  int
+	keep_going  int
+}
+
+/* Constants */
+const delim = '\n'
 
 func adder(x int, y int) int {
 	return x + y
@@ -28,6 +40,23 @@ func process_bag(bag_status int) int {
 	}
 }
 
+/* Example of user Input
+r := bufio.NewReader(os.Stdin)
+fmt.Printf("Enter a string: ")
+line, err := r.ReadString(delim)
+if err != nil {
+	fmt.Println(err)
+	os.Exit(1)
+}
+fmt.Printf(line) */
+
 func Run() {
-	fmt.Printf("Kittens\n")
+	current_player := Player{0, 0, 0, 1}
+	fmt.Println("Player is: ", current_player)
+
+	r := bufio.NewReader(os.Stdin)
+	fmt.Printf("Coffee Maker Quest 2.0\n")
+
+	r.ReadString(delim)
+
 }
